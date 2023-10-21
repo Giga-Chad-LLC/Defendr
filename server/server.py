f3b2bfacbbea425b6f69a60dcef8318b4f47106c
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-10-22 02:19:36 +0300

    Implement CORS middleware and update post request for users

--- server/server.py
+++ server/server.py
@@ -1,12 +1,24 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.src.routers import users, infoboxes
 
 try:
     app = FastAPI()
+
     app.include_router(users.router)
     app.include_router(infoboxes.router)
+
+    # required for the CORS pocilies
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        expose_headers=["Access-Control-Allow-Origin"]
+    )
 
     print("Server started")
 except Exception:
