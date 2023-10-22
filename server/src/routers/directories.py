a192803ecda6c30d90c37a63b96900cbb2899f4e
Vladislav Naumkin <naumkinvs@yandex.ru>
2023-10-22 19:08:56 +0200

    Implement directories API

--- server/src/routers/directories.py
+++ server/src/routers/directories.py
@@ -17,8 +17,24 @@ router = APIRouter(
 # @descr: get all directories
 @router.get("/", response_model=DirectoriesResponse, response_description="List of all directories")
 def get_directories(connection=Depends(get_connection)):
-    directories = []
-    return DirectoriesResponse(directories=directories)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM directories")
+        directories = []
+
+        for row in cursor.fetchall():
+            directories.append(DirectoryResponse(
+                id=row[0],
+                user_id=row[1],
+                title=row[2],
+                icon=row[3],
+            ))
+
+        return DirectoriesResponse(directories=directories)
+    
+    finally:
+        cursor.close()
 
 
 
@@ -28,7 +44,25 @@ def get_directories(connection=Depends(get_connection)):
 # @descr: get directories by user id
 @router.get("/{user_id}", response_model=DirectoriesResponse)
 def get_directory_by_id(user_id: int, connection=Depends(get_connection)):
-    return DirectoriesResponse(directories=[])
+    cursor = connection.cursor()
+    try:
+        query = "SELECT id, user_id, title, icon FROM directories WHERE user_id = %s"
+        cursor.execute(query, (user_id,))
+
+        rows = cursor.fetchall()
+        directories = []
+
+        for row in rows:
+            directories.append(DirectoryResponse(
+                id=row[0],
+                user_id=row[1],
+                title=row[2],
+                icon=row[3],
+            ))
+
+        return DirectoriesResponse(directories=directories)
+    finally:
+        cursor.close()
 
 
 
@@ -37,9 +71,28 @@ def get_directory_by_id(user_id: int, connection=Depends(get_connection)):
 # @descr: create directory in database
 @router.post("/", response_model=DirectoryResponse)
 def create_directory(directory: DirectoryDto, connection=Depends(get_connection)):
-    return DirectoryResponse(
-        id=-1,
-        user_id=-1,
-        title="title",
-        icon="icon"
-    )
+    if not directory.user_id or not directory.title or not directory.icon:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="All fields must be filled")
+    
+    cursor = connection.cursor()
+    try: 
+        query = "INSERT INTO directories (user_id, title, icon) VALUES (%s, %s, %s);"
+
+        cursor.execute(query, (directory.user_id, directory.title, directory.icon))
+
+        connection.commit()
+
+        directory_id = cursor.lastrowid
+        
+        return DirectoryResponse(
+            id=directory_id,
+            user_id=directory.user_id,
+            title=directory.title,
+            icon=directory.icon
+        )
+    except Exception as err:
+        print(err)
+        connection.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(err))
+    finally:
+        cursor.close()
