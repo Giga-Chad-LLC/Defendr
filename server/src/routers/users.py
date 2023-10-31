933a7890dc9a9ebe6086f97aaa0c3c15430e0637
Vladislav Artiukhov <vladislav0art.work@gmail.com>
2023-10-31 12:36:46 +0200

    Implement /users/top route

--- server/src/routers/users.py
+++ server/src/routers/users.py
@@ -95,7 +95,8 @@ def create_user(user: UserDto, connection=Depends(get_connection)):
 # @method: GET
 # @route: /users/layouts
 # @descr: return count of infoboxes with the provided layout for all users
-@router.get("/layouts", response_model=UsersInfoboxCountResponse, response_description="Count of infoboxes with the provided layout for all users")
+@router.get("/layouts", response_model=UsersInfoboxCountResponse,
+            response_description="Count of infoboxes with the provided layout for all users")
 def get_users_infobox_count_by_layout(layout: str, connection=Depends(get_connection)):
     if layout not in available_infobox_layouts:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
@@ -116,6 +117,56 @@ def get_users_infobox_count_by_layout(layout: str, connection=Depends(get_connec
         """
 
         cursor.execute(query, (layout,))
+
+        rows = cursor.fetchall()
+
+        data = []
+        for (user_id, infobox_layout, infobox_count) in rows:
+            data.append(UserInfoboxCountResponse(
+                user_id=user_id,
+                infobox_layout=infobox_layout,
+                infobox_count=infobox_count
+            ))
+
+        return UsersInfoboxCountResponse(data=data)
+    finally:
+        cursor.close()
+
+
+
+
+# @method: GET
+# @route: /users/top
+# @descr: return top N users that have the maximum number of infoboxes of the provided layout
+@router.get("/top", response_model=UsersInfoboxCountResponse,
+            response_description="Top 5 users that have the maximum number of infoboxes of the provided layout")
+def get_top_users_with_max_infobox_count_by_layout(layout: str, limit: int, connection=Depends(get_connection)):
+    if layout not in available_infobox_layouts:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Layout must be one of {', '.join(available_infobox_layouts.values())}, got '{layout}'")
+
+    if limit <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Limit must be positive number, got '{limit}'")
+
+    cursor = connection.cursor()
+    try:
+        query = """
+            SELECT user_id, infobox_layout, infobox_count
+            FROM (
+                SELECT
+                    u.id AS user_id,
+                    u.email AS user_email,
+                    i.layout AS infobox_layout,
+                    COUNT(i.id) AS infobox_count
+                FROM users u
+                LEFT JOIN infoboxes i ON u.id = i.user_id AND i.layout = %s
+                GROUP BY u.id, u.email
+                ORDER BY infobox_count DESC
+                LIMIT %s
+            ) AS top_users;
+        """
+
+        cursor.execute(query, (layout, limit))
 
         rows = cursor.fetchall()
 
